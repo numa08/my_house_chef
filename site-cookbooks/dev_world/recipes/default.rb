27a631c87b0fc02a7cea182ee1af4738fd4c8f8f
numa08 <n511287@gmail.com>
2013-10-03 16:38:50 +0000

    Gitとかのインストール

--- site-cookbooks/dev_world/recipes/default.rb
+++ site-cookbooks/dev_world/recipes/default.rb
@@ -6,3 +6,62 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+#
+
+# Install VCS
+# The Time is GIT!!!
+
+%{git subversion mercurial}.each do |pkg|
+  package pkg do
+    action :install
+  end
+end
+
+# Application version controll system
+# stow
+
+package "stow" do
+  action :install
+end
+
+directory "/usr/local/stow" do
+  owner root
+  group root
+  mode 0755
+  action :create
+  no_if { File.exists "/usr/local/stow" }
+end
+
+# Build latest version git
+
+%{make gcc curl-devel expat-devel gettext-devel openssl-devel zlib-devel}.each do |pkg|
+  package pkg do
+    action :install
+  end
+end
+
+directory "/usr/local/src" do
+  owner root
+  group root
+  mode 0755
+  action :create
+  no_if { File.exists "/usr/local/src"}
+end
+
+git "/usr/local/src/git" do
+  repository "git://git.kernel.org/pub/scm/git/git.git"
+  reference "master"
+  action :checkout
+end
+
+bash "install git" do
+  cwd '/usr/local/src/git'
+  code <<-EOC
+   make prefix=/usr/local/stow/git all
+   make prefix=/usr/local/stow/git install
+  EOC
+  cwd '/usr/local/stow'
+  code <<-EOC
+   stow -v git
+  EOC
+end
