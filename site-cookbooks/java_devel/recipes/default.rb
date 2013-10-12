39ad340a455a271d889ed0a156a8c96ffa700690
numa08 <n511287@gmail.com>
2013-10-12 09:11:55 +0900

    antをチェックアウトする

--- site-cookbooks/java_devel/recipes/default.rb
+++ site-cookbooks/java_devel/recipes/default.rb
@@ -5,4 +5,12 @@
 # Copyright 2013, YOUR_COMPANY_NAME
 #
 # All rights reserved - Do Not Redistribute
-#
+
+
+git "/usr/local/src/ant" do
+  repository "git://git.apache.org/ant.git"
+  reference "#{node.ant.version}"
+  action :checkout
+  not_if {File.exists?('/usr/local/bin/ant')}
+end
+
