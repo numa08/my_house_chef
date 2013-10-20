3be65f6cc20ce726e3eef8f02864d68ccb86f564
numa08 <n511287@gmail.com>
2013-10-20 18:49:37 +0900

    java関連の設定を記述

---
+++ roles/base.rb
@@ -0,0 +1,10 @@
+default_attributes(
+  :java => {
+    :oracle => {
+      "accept_oracle_download_terms" => true
+    },
+    "jdk_version" => '7',
+    "install_flavor" => "oracle",
+    "java_home" => "/usr/java/default"
+  }
+)
