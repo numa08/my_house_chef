469740b86c808b74b23894ed6c9f306f22f5a406
funabiki.t <funabiki.t@isoroot.jp>
2013-11-12 12:29:51 +0900

    Gnomeデスクトップ環境とVNCのインストール

--- site-cookbooks/desktop/recipes/default.rb
+++ site-cookbooks/desktop/recipes/default.rb
@@ -6,3 +6,34 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+#
+bash "install desktop" do
+  code <<-EOC
+    yum -y groupinstall "X Window System" Desktop
+    yum -y groupinstall "Japanese Support"
+  EOC
+end
+
+package "tigervnc-server" do
+  action :install
+end
+
+package "xinetd" do
+  action :install
+end
+
+cookbook_file "/etc/inittab" do
+end
+
+cookbook_file "/etc/xinetd.d/vnc-server" do
+end
+
+cookbook_file "/etc/gdm/custom.conf" do
+end
+
+bash "chkconfig setting" do
+  code <<-EOC
+   chkconfig vnc-server off
+   chkconfig xinetd on
+ EOC
+end
