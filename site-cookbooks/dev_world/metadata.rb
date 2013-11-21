f3e141f310ae847c79705889f58d88106610bd76
numa08 <n511287@gmail.com>
2013-11-22 00:27:46 +0900

    メタデータに俺のHNとメールアドレスを記述

--- site-cookbooks/dev_world/metadata.rb
+++ site-cookbooks/dev_world/metadata.rb
@@ -1,6 +1,6 @@
 name             'dev_world'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
+maintainer       'numa08'
+maintainer_email 'n511287@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures dev_world'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
